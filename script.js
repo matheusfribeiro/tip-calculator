7a54da72e5e6f340305b40ae9bbd37cc576b0910
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-02-05 20:29:13 -0300

    script logic and scss updates

--- script.js
+++ script.js
@@ -1,33 +1,105 @@
-let splitresult = document.querySelector('#splitresult')
-let totalresult = document.querySelector('#totalresult')
+let billInput = document.querySelector('#bill')
+let numberofpeopleInput = document.querySelector('#numberofpeople')
+let tips = document.querySelectorAll('.tips')
+let tipPerson = document.querySelector('#tipPerson')
+let totalPerson = document.querySelector('#totalPerson')
+let tipCustom = document.querySelector('#tipcustom')
+let resetButton = document.querySelector('#reset')
+let spanError = document.querySelector('#spanError')
+
+tipPerson.innerHTML = '$' + (0).toFixed(2)
+totalPerson.innerHTML = '$' + (0).toFixed(2)
 
 // BILL
-let billInput = document.querySelector('#bill')
-billInput.onblur = function getBill() {
-  bill = billInput.value
-  console.log(bill)
-  return bill
+billInput.value = '0'
+billInput.oninput = getBill
+
+let bill = 0
+function getBill() {
+  bill = parseFloat(billInput.value)
+  calculate()
 }
 
 
 // SELECT TIP
-let tipbutton = document.querySelectorAll('[data-tip]')
 
-tipbutton.forEach((tip) => {
-  return tip.addEventListener('click', event => console.log(tip.value))
+let tipPerc = 0.15
 
-  
+tips.forEach(function(item) {
+  item.addEventListener('click', clickFunc)
 })
+
+function clickFunc (e) {
+  tips.forEach(function(item) {
+    item.classList.remove('active')
+
+    if(e.target.innerHTML == item.innerHTML) {
+      item.classList.add('active')
+      tipPerc = parseFloat(item.innerHTML) / 100
+    }  
+  })
+
+  calculate()
+}
+
+tipCustom.oninput = customFunc
+
+ function customFunc () {
+  tipPerc = parseFloat(tipCustom.value / 100) 
+
+  tips.forEach(function(item) {
+    item.classList.remove('active')
+  })
+  calculate()
+ }
 
 
 //NUMBER OF PEOPLE
-let numberofpeopleInput = document.querySelector('#numberofpeople')
-numberofpeopleInput.onblur = function getPeople() {
-  numberofpeople = numberofpeopleInput.value
-  console.log(numberofpeople)
-  return numberofpeople
+numberofpeopleInput.value = '1'
+numberofpeopleInput.oninput = getPeople
+
+let numberofpeople = 1
+function getPeople() {
+  numberofpeople = parseFloat(numberofpeopleInput.value)
+  calculate()
+
+  if (numberofpeople < 1) {
+    numberofpeopleInput.classList.add('error')
+    spanError.style.display = 'flex'
+  } else {
+    numberofpeopleInput.classList.remove('error')
+    spanError.style.display = 'none'
+    calculate()
+  }
 }
 
+
+//CALCULATE
+
+function calculate() {
+  if (numberofpeople >= 1) {
+    let tipAmount = (bill * tipPerc) / numberofpeople
+    let total = (bill + tipAmount) / numberofpeople
+    tipPerson.innerHTML = '$' + tipAmount.toFixed(2)
+    totalPerson.innerHTML = '$' + total.toFixed(2)
+  }
+}
+
+//RESET
+
+resetButton.onclick = reset
+
+function reset() {
+  tipPerson.innerHTML = '$' + (0).toFixed(2)
+  totalPerson.innerHTML = '$' + (0).toFixed(2)
+  billInput.value = '0'
+  getBill()
+  numberofpeopleInput.value = 1
+  getPeople()
+  tipCustom.value = '0'
+
+
+}
 
 
 
